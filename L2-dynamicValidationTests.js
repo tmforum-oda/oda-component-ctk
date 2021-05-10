2f8dbc7ba18df9a90f38e24d713d15c397227811
Lester Thomas <lester.thomas@vodafone.com>
2021-05-10 21:27:36 +0100

    Initial working version of L2-Dynamic

---
+++ L2-dynamicValidationTests.js
@@ -0,0 +1,161 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+const fs = require('fs')
+const chai = require('chai')
+const YAML = require('yaml')
+const process = require('process')
+const k8s = require('@kubernetes/client-node')
+var chaiFiles = require('chai-files');
+chai.use(chaiFiles)
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+const expect = chai.expect
+const COMPONENT = 'component'
+
+const ctkPaths = {
+  "Product Catalog Management": "TMF620_Product_catalog_V4-0-0",
+  "Party Role Management": "TMF669-PartyRole"
+}
+const kc = new k8s.KubeConfig()
+kc.loadFromDefault()
+
+console.log('***************************************************************************')
+console.log('Open Digital Architecture - Component Test Kit CTK Level 2 Dynamic Tests')
+console.log('***************************************************************************')
+console.log()
+
+const components = process.env.components.split(',')
+for (const index in components) {
+  const componentEnvelopeName = components[index]
+  let documentArray = []
+  const file = fs.readFileSync(componentEnvelopeName, 'utf8')
+  documentArray = YAML.parseAllDocuments(file)
+  const componentDoc = getComponentDocument(documentArray)
+  const componentAPIVersion = componentDoc.get('apiVersion')
+  const metadata = componentDoc.get('metadata')
+  const componentName = metadata.get('name')
+  const k8sCoreApi = kc.makeApiClient(k8s.CoreV1Api)
+  const k8sCustomApi = kc.makeApiClient(k8s.CustomObjectsApi)
+  describe('Step 0: Basic environment connectivity tests', function () {
+    it('Kubectl configured correctly', function (done) {
+      k8sCoreApi.listNamespacedPod('components').then((res) => {
+        expect(res, "Kubectl should return pods in 'components' namespace").to.be.a('object')
+        done()
+      })
+    })
+  })
+
+  describe('Step 1: Check metadata for component ' + componentEnvelopeName, function () {
+    it('Component can be found', function (done) {
+      k8sCustomApi.listNamespacedCustomObject('oda.tmforum.org', 'v1alpha3', 'components', 'components', undefined, undefined, 'metadata.name=' + componentName)
+        .then(function (res) {
+          const numberOfComponentsFound = res.body.items.length
+          expect(numberOfComponentsFound, 'Should find 1 component with name ' + componentName).to.equal(1)
+          done()
+        }).catch(done)
+    })
+
+    it('Component has deployed successfully (summary/status.deployment_status: Complete)', function (done) {
+      k8sCustomApi.listNamespacedCustomObject('oda.tmforum.org', 'v1alpha3', 'components', 'components', undefined, undefined, 'metadata.name=' + componentName)
+        .then(function (res) {
+          const status = res.body.items[0].status
+          expect(status['summary/status'].deployment_status, 'status.summary/status.deployment_status is Complete').to.equal('Complete')
+          done()
+        }).catch(done)
+    })
+  })
+
+
+
+  // get Component resource
+  k8sCustomApi.listNamespacedCustomObject('oda.tmforum.org', 'v1alpha3', 'components', 'components', undefined, undefined, 'metadata.name=' + componentName).then(function (res) {
+    describe('Step 2 run CTK on each mandatory API from Golden Component ', function () {
+      const status = res.body.items[0].status
+      const spec = res.body.items[0].spec
+      const type = spec.type
+      const version = spec.version
+      const goldenComponentFilename = type + '-v' + version.split('.').join('-') + '.yaml'
+      const file = fs.readFileSync('./golden-components/' + goldenComponentFilename, 'utf8')
+      documentArray = YAML.parseAllDocuments(file)
+      const goldenComponent = documentArray[0]
+      const goldenExposedAPI = goldenComponent.get('spec').get('coreFunction').get('exposedAPIs')
+      const goldenExposedAPIArray = goldenExposedAPI.items
+      const exposedAPIArray = spec.coreFunction.exposedAPIs
+      for (const key in goldenExposedAPIArray) {
+        const goldenAPIName = goldenExposedAPIArray[key].get('name')
+        it("Executing OpenAPI CTK for " + goldenAPIName + ": check /results folder for your results.", async function () {
+          try {
+            this.timeout(120000) // 2 minute timeout
+
+            const goldenAPISpec = goldenExposedAPIArray[key].get('specification')
+            const goldenAPIobject = await getSchemaFromURL(goldenAPISpec)
+            // look in the current component spec for an API with the same title and version
+            let foundAPI = false
+            let targetCTKTitle, targetCTKVersion, targetAPIName
+            for (const exposedAPIArrayKey in exposedAPIArray) {
+              const exposedAPISpec = exposedAPIArray[exposedAPIArrayKey].specification
+              const exposedAPIobject = await getSchemaFromURL(exposedAPISpec)
+              if ((exposedAPIobject.info.title === goldenAPIobject.info.title) && (exposedAPIobject.info.version === goldenAPIobject.info.version)) {
+                foundAPI = true
+                targetCTKTitle = goldenAPIobject.info.title
+                targetCTKVersion = goldenAPIobject.info.version
+                targetAPIName = componentName + '-' + exposedAPIArray[exposedAPIArrayKey].name
+              }
+            }
+            expect(foundAPI, "Found '" + goldenAPIobject.info.title + "' API with version '" + goldenAPIobject.info.version + "'").to.equal(true)
+            const ctkName = ctkPaths[targetCTKTitle]
+            config = JSON.parse(fs.readFileSync('./api-ctk/' + ctkName + '/config.json'))
+            // update the API URL in config from Component
+            // get url from component status
+            let targetAPIURL
+            for (const statusPIKey in status.exposedAPIs) {
+              if (status.exposedAPIs[statusPIKey].name === targetAPIName) {
+                targetAPIURL = status.exposedAPIs[statusPIKey].url
+              }
+            }
+            config.url = targetAPIURL + '/'
+            fs.writeFileSync('./api-ctk/' + ctkName + '/config.json', JSON.stringify(config))
+
+            const { execSync  } = require('child_process');
+            execSync ('npm start', {cwd: './api-ctk/' + ctkName + '/ctk'})
+            let oldPath = './api-ctk/' + ctkName + '/htmlResults.html'
+            let newPath = './results/' + ctkName + '.html'
+            fs.rename(oldPath, newPath, function (err) {
+              if (err) throw err
+            })
+            oldPath = './api-ctk/' + ctkName + '/jsonResults.json'
+            newPath = './results/' + ctkName + '.json'
+            fs.rename(oldPath, newPath, function (err) {
+              if (err) throw err
+            })
+            // For each CTK, edit the config.json file and run the CTK
+            // Copy and re-name the htmlResults and jsonResults files
+            expect({}, 'Test should be of type object').to.be.a('object')
+          }
+          catch (error) {
+            console.log(error)
+          }
+        })
+      }
+    })
+  })
+}
+
+function getComponentDocument (inDocumentArray) {
+  // go through each document checking for a kind: component
+  for (const docKey in inDocumentArray) {
+    if (inDocumentArray[docKey].get('kind') === COMPONENT) {
+      return inDocumentArray[docKey]
+    }
+  }
+  return null
+}
+
+async function getSchemaFromURL(inURL) {
+  const httpScheme = inURL.split('://')[0] + '://'
+  const server = inURL.split('://')[1].split('/')[0]
+  const apiPath = '/' + inURL.split('://')[1].split(/\/(.+)/)[1]
+  const APIresponse = await chai.request(httpScheme + server).get(apiPath).send()
+  const APIobject = JSON.parse(APIresponse.res.text)
+  return (APIobject)
+}
