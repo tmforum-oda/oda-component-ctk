4aa23691198943e48119b882861b68709a9f8ed8
Lester Thomas <lester.thomas@vodafone.com>
2022-03-04 22:09:41 +0000

    1st scenario complete

--- BDDandTDD/TMFC001-productcatalogmanagement/component-utils/component-utils.js
+++ BDDandTDD/TMFC001-productcatalogmanagement/component-utils/component-utils.js
@@ -1,51 +1,61 @@
 const k8s = require('@kubernetes/client-node')
 const fs = require('fs')
 const YAML = require('yaml')
-const process = require('process')
+const axios = require('axios')
 
-const COMPONENT = 'component'
-const NAMESPACE = 'components'
-const HEADER = process.env.HEADER
 const kc = new k8s.KubeConfig()
 kc.loadFromDefault()
-const componentEnvelopeName = "../../v1alpha4.component.yaml"
-let documentArray = []
-const file = fs.readFileSync(componentEnvelopeName, 'utf8')
-documentArray = YAML.parseAllDocuments(file)
-const componentDoc = getComponentDocument(documentArray)
 
-function getComponentDocument (inDocumentArray) {
-  // go through each document checking for a kind: component
-  for (const docKey in inDocumentArray) {
-    if (inDocumentArray[docKey].get('kind') === COMPONENT) {
-      return inDocumentArray[docKey]
-    }
-  }
-  return null
-}
-
+const HTTP_CREATED = 201
 const componentUtils = {
-  getCoreAPIs: async function () {
-    const metadata = componentDoc.get('metadata')
-    const componentName = metadata.get('name')
+  getAPIURL: async function (inComponentInstance, inAPIName, inNamespace) {
     const k8sCustomApi = kc.makeApiClient(k8s.CustomObjectsApi)
-    const namespacedCustomObject = await k8sCustomApi.listNamespacedCustomObject('oda.tmforum.org', 'v1alpha4', NAMESPACE, 'components', undefined, undefined, 'metadata.name=' + componentName)
-    var exposedAPIList = namespacedCustomObject.body.items[0].status.exposedAPIs
-    return exposedAPIList
+    const APIResourceName = inComponentInstance + '-' + inAPIName
+    const namespacedCustomObject = await k8sCustomApi.listNamespacedCustomObject('oda.tmforum.org', 'v1alpha4', inNamespace, 'apis', undefined, undefined, 'metadata.name=' + APIResourceName)
+    if (namespacedCustomObject.body.items.length === 0) {
+      return null // API not found
+    }
+    var APIURL = namespacedCustomObject.body.items[0].status.apiStatus.url
+    return APIURL
   },
   loadTestData: async function (inTestDataFile, inAPIURL) {
     const testData = JSON.parse(fs.readFileSync(inTestDataFile, 'utf8'))
-    console.log('============================================================')
-    for(const payload in testData.payloads) {
+    let loadedSuccessfully = true
+    for (const payload in testData.payloads) {
       const resourceURL = inAPIURL + '/' + payload
-      console.log(payload)
-      console.log(resourceURL)
       for (const index in testData.payloads[payload]) {
         const body = testData.payloads[payload][index].payload
-        console.log(body)
+        const response = await axios.post(resourceURL, body)
+        if (response.status !== HTTP_CREATED) {
+          loadedSuccessfully = false
+        }
       }
     }
-    console.log('============================================================')
+    return loadedSuccessfully
+  },
+  validateReturnData: async function (inTestDataFile, inReturnData) {
+    const testData = JSON.parse(fs.readFileSync(inTestDataFile, 'utf8'))
+    let validatedSuccessfully = true
+
+    // go through each item in test data and compare to return data
+    for(const payload in testData.payloads) {
+      for (const index in testData.payloads[payload]) {
+        const body = testData.payloads[payload][index].payload
+
+        // compare body.name and body.description to return data
+        let found = false
+        for (const returnDataItem in inReturnData) {
+          if ((inReturnData[returnDataItem].name === body.name) && (inReturnData[returnDataItem].description === body.description)) {
+            found = true
+            break
+          }
+        }
+        if (!found) {
+          validatedSuccessfully = false
+        }
+      }
+    }
+    return validatedSuccessfully
   }
 }
 module.exports = componentUtils
--- BDDandTDD/TMFC001-productcatalogmanagement/features/steps/IG1228-UC002-Steps.v2.js
+++ BDDandTDD/TMFC001-productcatalogmanagement/features/steps/IG1228-UC002-Steps.v2.js
@@ -1,42 +1,59 @@
 const { Given, When, Then, After } = require('@cucumber/cucumber');
-const componentUtils = require('component-utils')
 const fs = require('fs')
 const chai = require('chai')
 const chaiHttp = require('chai-http')
+const assert = require('assert');
 const YAML = require('yaml')
-const process = require('process')
 const k8s = require('@kubernetes/client-node')
+const componentUtils = require('component-utils')
 
 chai.use(chaiHttp)
 const expect = chai.expect
 const COMPONENT = 'component'
-const NAMESPACE = process.env.NAMESPACE
-const HEADER = process.env.HEADER
+const NAMESPACE = 'components'
+const TEST_DATA_FILE = './test-data/product-categories.config.json'
+const HTTP_OK = 200
+
 const kc = new k8s.KubeConfig()
 kc.loadFromDefault()
 
-Given('An initial catalog populated with product category data', async function () {
-  // Write code here that turns the phrase above into concrete actions
+const componentInstanceName = 'r1-productcatalog'
 
+function getComponentDocument (inDocumentArray) {
+  // go through each document checking for a kind: component
+  for (const docKey in inDocumentArray) {
+    if (inDocumentArray[docKey].get('kind') === COMPONENT) {
+      return inDocumentArray[docKey]
+    }
+  }
+  return null
+}
+
+Given('An initial catalog populated with product category data', async function () {
   // get access to the Product Catalog API
-  console.log('Getting access to the Product Catalog API')
-  var apiList = await componentUtils.getCoreAPIs()
-  console.log(apiList)
-  await componentUtils.loadTestData('./test-data/product-categories.config.json', apiList[0].url)
+  const productCatalogAPIURL = await componentUtils.getAPIURL(componentInstanceName, 'productcatalogmanagement', NAMESPACE)
+  assert.notEqual(productCatalogAPIURL, null, "Can't find Product Catalog API")
 
   // get the initial data from the product-categories.config.json file
-  
-  return 'pending'
+  const loadSuccessful = await componentUtils.loadTestData(TEST_DATA_FILE, productCatalogAPIURL)
+  assert.equal(loadSuccessful, true, 'Failed to load test data')
 })
 
 When('we request the products categories', async function () {
-  // write implementation here
-  return 'pending'
+  // get access to the Product Catalog API
+  const productCatalogAPIURL = await componentUtils.getAPIURL(componentInstanceName, 'productcatalogmanagement', NAMESPACE)
+  assert.notEqual(productCatalogAPIURL, null, "Can't find Product Catalog API")
+
+  const response = await chai.request(productCatalogAPIURL).get('/category')
+  assert.equal(response.status, HTTP_OK, 'Failed to get product categories')
+
+  this.returnData = response.body
 })
 
 Then('we should receive list of categories', async function (dataTable) {
-  // write implementation here
-  return 'pending'
+  // check the response matches the test data
+  const valid = await componentUtils.validateReturnData(TEST_DATA_FILE, this.returnData)
+  assert.equal(valid, true, 'Product categories do not match test data')
 })
 
 Given('An initial catalog populated with product offer data linked to {string} category', function (string) {
