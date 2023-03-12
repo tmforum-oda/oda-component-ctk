655f194a31a6c96de659e85e8c3f7daa31ef6261
Victor Mari Rodriguez <vrodriguez@tmforum.org>
2023-03-12 02:59:56 +0100

    Automated running tests

--- reporter.sh
+++ reporter.sh
@@ -7,4 +7,11 @@
 npm run L1-static r1-productcatalog.component.yaml 
 npm run L1-dynamic r1-productcatalog.component.yaml 
 npm run L2-static r1-productcatalog.component.yaml 
-npm run L2-dynamic r1-productcatalog.component.yaml 
+npm run L2-dynamic r1-productcatalog.component.yaml
+
+pushd BDDandTDD/TMFC001-productcatalogmanagement
+npm install && npm start
+
+popd
+pushd api-ctk/TMF620_Product_catalog_V4-0-0
+bash Mac-Linux-RUNCTK.sh
